003f6fc66a80da9ae9b535471e6f6cf36e2283f4
Magleo Medina <xxmagleoxx.medina2003@gmail.com>
2026-09-06 17:51:11 -0400

    fix: configure for Expo Go
    - app.json: add expo config block
    - package.json: main -> expo/AppEntry.js, add start:rnc script
    - metro.config.js: use expo/metro-config
    - babel.config.js: use babel-preset-expo

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,30 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
-const fs = require('fs');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const webHtml = fs.readFileSync(path.join(__dirname, 'web', 'index.html'), 'utf8');
+const config = getDefaultConfig(__dirname);
 
-function resolveRequest(context, moduleName, platform) {
-  if (platform === 'web' && moduleName === 'react-native') {
-    return context.resolveRequest(context, 'react-native-web', platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-}
-
-const config = {
-  resolver: {
-    resolveRequest,
-  },
-  server: {
-    enhanceMiddleware: (middleware) => (req, res, next) => {
-      if (req.url === '/web' || req.url === '/web/') {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(webHtml);
-        return;
-      }
-      return middleware(req, res, next);
-    },
-  },
-};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
